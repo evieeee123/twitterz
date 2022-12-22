ab21fdc2ce8af2616b6ebdd8d07e41726302e72b
Evie <evie.zengyiying@gmail.com>
2022-12-22 15:54:47 -0800

    create feed component

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Feed from '../components/Feed'
 import Sidebar from '../components/Sidebar'
 
 
@@ -18,7 +19,7 @@ export default function Home() {
         <Sidebar />
 
         {/* feed */}
-        
+        <Feed />
 
         {/* widgets */}
 
