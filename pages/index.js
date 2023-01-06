8bc0ecddc8b0335a82453097b8d0bcaa71c1439e
Evie <evie.zengyiying@gmail.com>
2023-01-05 21:25:28 -0800

    created widget component and search bar

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Feed from '../components/Feed'
 import Sidebar from '../components/Sidebar'
+import Widgets from '../components/Widgets'
 
 
 
@@ -22,6 +23,7 @@ export default function Home() {
         <Feed />
 
         {/* widgets */}
+        <Widgets />
 
         {/* modal */}
 
