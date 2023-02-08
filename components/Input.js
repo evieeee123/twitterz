cfcdcd7266d6634717d86064797076c6907c5006
Evie <evie.zengyiying@gmail.com>
2023-02-07 20:04:19 -0800

    add loading effect

--- components/Input.js
+++ components/Input.js
@@ -11,10 +11,15 @@ import { getDownloadURL, uploadString, ref} from "firebase/storage";
 export default function Input() {
   const {data: session} = useSession();
   const [input, setInput] = useState("");
-  const [selectedFile, setSelectedFile] = useState(null)
+  const [selectedFile, setSelectedFile] = useState(null);
+  const [loading, setLoading] = useState(false)
   const filePickerRef = useRef(null);
 
   const sendPost = async () => {
+
+    if(loading) return;
+    setLoading(true)
+
     const docRef = await addDoc(collection(db, "posts"), {
       id: session.user.uid,
       text: input,
@@ -36,7 +41,8 @@ export default function Input() {
     }
 
     setInput("");
-    setSelectedFile(null)
+    setSelectedFile(null);
+    setLoading(false)
   }
 
   const addImageToPost = (e) => {
@@ -66,18 +72,22 @@ export default function Input() {
               {selectedFile && (
                 <div className="relative">
                   <XIcon onClick={() => setSelectedFile(null)} className="h-7 text-black absolute cursor-pointer shadow-md shadow-white rounded-full" />
-                  <img src={selectedFile} alt="" />
+                  <img src={selectedFile} className={`${loading && "animate-pulse"}`} />
                 </div>
               )}
               <div className="flex items-center justify-between pt-2.5">
-                  <div className="flex">
-                    <div className="" onClick={() => filePickerRef.current.click()}>
-                      <PhotographIcon className="h-10 w-10 hoverEffect p-2 text-sky-500 hover:bg-sky-100" />
-                      <input type="file" hidden ref={filePickerRef} onChange={addImageToPost} />
+                {!loading && (
+                  <>
+                    <div className="flex">
+                      <div className="" onClick={() => filePickerRef.current.click()}>
+                        <PhotographIcon className="h-10 w-10 hoverEffect p-2 text-sky-500 hover:bg-sky-100" />
+                        <input type="file" hidden ref={filePickerRef} onChange={addImageToPost} />
+                      </div>
+                        <EmojiHappyIcon className="h-10 w-10 hoverEffect p-2 text-sky-500 hover:bg-sky-100" />
                     </div>
-                      <EmojiHappyIcon className="h-10 w-10 hoverEffect p-2 text-sky-500 hover:bg-sky-100" />
-                  </div>
-                  <button onClick={sendPost} disabled={!input.trim()} className="bg-blue-400 text-white px-4 py-1.5 rounded-full font-bold shadow-md hover:brightness-95 disabled:opacity-50">Tweet</button>
+                    <button onClick={sendPost} disabled={!input.trim()} className="bg-blue-400 text-white px-4 py-1.5 rounded-full font-bold shadow-md hover:brightness-95 disabled:opacity-50">Tweet</button>
+                  </>
+                )}
               </div>
             </div>
       </div>
