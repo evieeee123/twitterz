76aef466c25b8851a510a4a7539fcfff13e48a79
Evie <evie.zengyiying@gmail.com>
2023-02-15 21:04:05 -0800

    fix the posts timestamp and add like functionality

--- components/Post.js
+++ components/Post.js
@@ -1,8 +1,18 @@
 import { ChartBarIcon, ChatIcon, DotsHorizontalIcon, HeartIcon, ShareIcon, TrashIcon } from '@heroicons/react/outline'
 import React from 'react'
-import Moment from 'react-moment'
+import Moment from 'react-moment';
+import { setDoc, doc } from 'firebase/firestore';
+import { useSession } from 'next-auth/react';
+import { db } from '../firebase';
 
 export default function Post({post}) {
+  const {data: session} = useSession()
+  async function likePost() {
+    await setDoc(doc(db, "posts", post.id, "likes", session.user.uid), {
+      usename: session.user.usename
+    })
+  }
+
   return (
     <div className='flex p-3 cursor-pointer border-b border-gray-200'>
         {/* image */}
@@ -18,7 +28,7 @@ export default function Post({post}) {
                     <span className='text-sm sm:text-[15px]'>@{post.data().username} - </span>
                     <span className='text-sm sm:text-[15px] hover:underline'>
                       {/* {post.timestamp} */}
-                      <Moment fromNow>{post?.timestamp?.toDate()}</Moment>
+                      <Moment fromNow>{post?.data().timestamp?.toDate()}</Moment>
                     </span>
                 </div>
                 {/* dot icon */}
@@ -33,7 +43,7 @@ export default function Post({post}) {
             <div className='flex justify-between text-gray-500 p-2'>
                 <ChatIcon className='h-9 w-9 hoverEffect p-2 hover:text-sky-500 hover:bg-sky-100' />
                 <TrashIcon className='h-9 w-9 hoverEffect p-2 hover:text-red-600 hover:bg-red-100' />
-                <HeartIcon className='h-9 w-9 hoverEffect p-2 hover:text-red-600 hover:bg-red-100' />
+                <HeartIcon onClick={likePost} className='h-9 w-9 hoverEffect p-2 hover:text-red-600 hover:bg-red-100' />
                 <ShareIcon className='h-9 w-9 hoverEffect p-2 hover:text-sky-500 hover:bg-sky-100' />
                 <ChartBarIcon className='h-9 w-9 hoverEffect p-2 hover:text-sky-500 hover:bg-sky-100' />
             </div>
