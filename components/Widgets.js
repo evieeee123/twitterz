72fe881f0780afa72768208ce45d0b882a1abc77
Evie <evie.zengyiying@gmail.com>
2023-03-23 20:20:37 -0700

    update news styling

--- components/Widgets.js
+++ components/Widgets.js
@@ -32,7 +32,7 @@ export default function Widgets({newsResults, randomUsersResults}) {
             <AnimatePresence>
                 {randomUsersResults.slice(0, randomUserNum).map((randomUser) => (
                     <motion.div key={randomUser.login.username} initial={{opacity: 0}} animate={{opacity: 1}} exit={{opacity: 0}} transition={{duration: 1}}>
-                        <div key={randomUser.login.username} className="flex items-center px-4 py-2 cursor-pointer hover:bg-gray-200">
+                        <div key={randomUser.login.username} className="flex items-center px-4 py-2 cursor-pointer hover:bg-gray-200 transition duration-500 ease-out">
                             <img className="rounded-full" width="40" src={randomUser.picture.thumbnail} />
                             <div className="truncate ml-4 leading-5">
                                 <h4 className="font-bold hover:underline text-[14px] truncate">{randomUser.login.username}</h4>
