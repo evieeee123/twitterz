99cf91972dd0e8cdb12136b727725f9411e6d1ed
Evie <evie.zengyiying@gmail.com>
2023-03-31 22:39:50 -0700

    update commentmodal

--- components/CommentModal.js
+++ components/CommentModal.js
@@ -1,6 +1,13 @@
+import { useRecoilState } from "recoil";
+import { modalState } from "../atom/modalAtom";
 
 export default function CommentModal() {
+    const [open, setOpen] = useRecoilState(modalState)
+
   return (
-    <div>CommentModal</div>
+    <div>
+        <h1>Comment Modal</h1>
+        {open && <h1>The modal is open</h1>}
+    </div>
   )
 }
