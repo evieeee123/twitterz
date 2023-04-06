5f083d29c6d42cd918ff501ed0eb8653fccf539d
Evie <evie.zengyiying@gmail.com>
2023-04-05 22:22:39 -0700

    styling the comment open window

--- components/CommentModal.js
+++ components/CommentModal.js
@@ -9,7 +9,10 @@ export default function CommentModal() {
     <div>
         <h1>Comment Modal</h1>
         {open && (
-            <Modal isOpen={open}>
+            <Modal 
+            isOpen={open}
+            onRequestClose={() => setOpen(false)}
+            className="max-w-lg h-[300px] w-[90%] absolute top-24 left-[50%] translate-x-[-50%] bg-white border-1 border-gray-200 rounded-xl shadow-md">
                 <h1>Comment Modal</h1>
             </Modal>
         )}
