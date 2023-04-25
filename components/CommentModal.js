d12ab12c765cd6ae2c893159799aa06ea58d9b91
Evie <evie.zengyiying@gmail.com>
2023-04-24 22:04:20 -0700

    update comment modal

--- components/CommentModal.js
+++ components/CommentModal.js
@@ -8,13 +8,15 @@ import { doc, onSnapshot } from "firebase/firestore";
 import { useState } from "react";
 import Moment from "react-moment";
 
-export default function CommentModal() {
+export default function CommentModal({post}) {
     const [open, setOpen] = useRecoilState(modalState);
     const [postId] = useRecoilState(postIdState);
-    const [post, setPost] = useState({});
+    const [pt, setPost] = useState({});
 
     useEffect(() => {
-        onSnapshot(doc(db, "posts", postId), (snapshot) => {(setPost(snapshot))})
+        onSnapshot(doc(db, "posts", postId), (snapshot) => {
+            (setPost(snapshot))
+        });
     }, [postId, db])
 
   return (
@@ -28,17 +30,25 @@ export default function CommentModal() {
                 <div className="p-1">
                     <div className="border-b border-gray-200 py-2 px-1.5">
                         <div onClick={() => setOpen(false)} className="hoverEffect h-9 w-9 flex items-center justify-center">
-                            <XIcon className="h-[22px] text-gray-700" />
+                            <XIcon className="h-[23px] text-gray-700 p-0" />
                         </div>
                     </div>
                       <div className="p-2 flex items-center space-x-1">
-                        <img className='h-11 w-11 rounded-full mr-4' src={post?.data()?.userImg} alt='user-image' />
-                        <h4 className='font-bold text-[15px] sm:text-[16px] hover:underline'>{post?.data()?.name}</h4>
-                        <span className='text-sm sm:text-[15px]'>@{post?.data()?.username} - </span>
-                        <span className='text-sm sm:text-[15px] hover:underline'>
-                            {/* {post.timestamp} */}
-                            <Moment fromNow>{post?.data().timestamp?.toDate()}</Moment>
-                        </span>
+                        <span className="w-0.5 h-full z-[-1] absolute left-8 top-11 bg-gray-300" />
+                          <img
+                              className="h-11 w-11 rounded-full mr-4"
+                              src={post?.data()?.userImg}
+                              alt="user-img"
+                          />
+                          <h4 className="font-bold text-[15px] sm:text-[16px] hover:underline">
+                              {post?.data()?.name}
+                          </h4>
+                          <span className="text-sm sm:text-[15px]">
+                              @{post?.data()?.username} -{" "}
+                          </span>
+                          <span className="text-sm sm:text-[15px] hover:underline">
+                              <Moment fromNow>{post?.data()?.timestamp?.toDate()}</Moment>
+                          </span>
                       </div>
                 </div>
             </Modal>
