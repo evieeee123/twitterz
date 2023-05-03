8f7fd0c4ec4177a6d1c41dfb01ebda85b0cae019
Evie <evie.zengyiying@gmail.com>
2023-05-02 22:31:23 -0700

    calculate how many comments

--- components/Post.js
+++ components/Post.js
@@ -14,6 +14,7 @@ import { modalState, postIdState } from '../atom/modalAtom';
 export default function Post({post}) {
   const {data: session} = useSession();
   const [likes, setLikes] = useState([]);
+  const [comments, setComments] = useState([]);
   const [hasliked, setHasLiked] = useState(false);
   const [open, setOpen] = useRecoilState(modalState);
   const [postId, setPostId] = useRecoilState(postIdState);
@@ -22,6 +23,12 @@ export default function Post({post}) {
   useEffect(() => {
     const unsubscribe = onSnapshot(
       collection(db, "posts", post.id, "likes"), (snapshot) => setLikes(snapshot.docs)
+    )
+  }, [db])
+
+  useEffect(() => {
+    const unsubscribe = onSnapshot(
+      collection(db, "posts", post.id, "comments"), (snapshot) => setComments(snapshot.docs)
     )
   }, [db])
 
@@ -78,15 +85,21 @@ export default function Post({post}) {
             <img className='rounded-2xl mr-2' src={post.data().image} alt='post image' />
               {/* icons */}
             <div className='flex justify-between text-gray-500 p-2'>
-                <ChatIcon onClick={() => { 
-                  if(!session) {
+              <div className=''>
+                <ChatIcon onClick={() => {
+                  if (!session) {
                     signIn()
                   } else {
                     setPostId(post.id)
-                    setOpen(!open); 
+                    setOpen(!open);
                   }
-                  }
+                }
                 } className='h-9 w-9 hoverEffect p-2 hover:text-sky-500 hover:bg-sky-100' />
+                {comments.length > 0 && (
+                  <span>{comments.length}</span>
+                )}
+              </div>
+                
                 {session?.user.uid === post?.data().id && (
                   <TrashIcon onClick={deletePost} className='h-9 w-9 hoverEffect p-2 hover:text-red-600 hover:bg-red-100' />
                 )}
