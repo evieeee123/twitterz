7ce46bae920f321a1364364f076ce989dd2d0f01
Evie <evie.zengyiying@gmail.com>
2023-05-08 21:45:50 -0700

    add feed section to post page

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -20,6 +20,13 @@ export default function Post({ newsResults, randomUsersResults }) {
                 <Sidebar />
 
                 {/* feed */}
+                <div className="xl:ml-[370px] border-l border-r xl:min-w-[576px] sm:ml-[73px] flex-grow max-w-xl border-gray-200">
+                    <div className="flex py-2 px-3 sticky top-0 z-50 bg-white border-b border-gray-200">
+                        <h2 className="text-lg sm:text-xl font-bold cursor-pointer">Home</h2>
+                        
+                    </div>
+                    
+                </div>
 
                 {/* widgets */}
                 <Widgets newsResults={newsResults.articles} randomUsersResults={randomUsersResults.results} />
