877c45c0d95891135bb573b15a42820bf4dda0af
Evie <evie.zengyiying@gmail.com>
2023-06-01 20:05:09 -0700

    redirect when click on the img

--- components/Post.js
+++ components/Post.js
@@ -85,9 +85,9 @@ export default function Post({post, id}) {
             </div>
 
               {/* post text */}
-            <p className='text-gray-700 text-[15px] sm:text-[16px] mb-2'>{post?.data()?.text}</p>
+            <p onClick= {() => router.push(`/posts/${id}`)} className='text-gray-700 text-[15px] sm:text-[16px] mb-2'>{post?.data()?.text}</p>
               {/* post image */}
-            <img className='rounded-2xl mr-2' src={post?.data()?.image} alt='post image' />
+            <img onClick={() => router.push(`/posts/${id}`)} className='rounded-2xl mr-2' src={post?.data()?.image} alt='post image' />
               {/* icons */}
             <div className='flex justify-between text-gray-500 p-2'>
               <div className='flex items-center select-none'>
